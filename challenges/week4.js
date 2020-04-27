a51fed15a474431416b0c13dd9074bb26a432aac
Saoirse Bryar <smbryar@gmail.com>
2020-04-27 09:28:56 +0100

    Implement getLongestSides

--- challenges/week4.js
+++ challenges/week4.js
@@ -41,7 +41,7 @@ function findSentencesContaining(sentences, str) {
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  return triangles.map(arr => Math.max(...arr));
 }
 
 module.exports = {
